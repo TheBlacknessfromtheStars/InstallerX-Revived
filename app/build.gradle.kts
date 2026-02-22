9e3f1dccc79bb1207c6f3e3bf6a85ad6a1ce11d3
TheBlacknessfromtheStars <109412448+TheBlacknessfromtheStars@users.noreply.github.com>
2026-02-22 11:01:52 +0800

    Update versionCode to 100 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         // Version control
         // Github Actions will automatically use versionName A.B.C+1 when building preview releases
         // update versionCode and versionName before manually trigger a stable release
-        versionCode = 45
+        versionCode = 100
         versionName = project.findProperty("VERSION_NAME") as String?
             ?: project.findProperty("BASE_VERSION") as String
 
